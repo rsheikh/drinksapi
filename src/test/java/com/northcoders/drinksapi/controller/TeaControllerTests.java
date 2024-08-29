c462b486bf0ec50a165bce721b050f3bcf8c7913
rsheikh <red.sheikh@gmail.com>
2024-08-29 12:43:10 +0100

    test: modified test class to add tests for URL with and without parameters

--- src/test/java/com/northcoders/drinksapi/controller/TeaControllerTests.java
+++ src/test/java/com/northcoders/drinksapi/controller/TeaControllerTests.java
@@ -1,5 +1,6 @@
 package com.northcoders.drinksapi.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -23,5 +24,33 @@ public class TeaControllerTests {
                 MockMvcRequestBuilders.get("/tea/tealovers"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string(expectedContent));
+    }
+
+    @Test
+    @DisplayName("Test tea to return default values when none are specified in the request")
+    public void testGetTeaDefault() throws Exception {
+        String type = "black";
+        String name = "Assam";
+
+        this.mockMvcController.perform(
+                        MockMvcRequestBuilders.get("/tea"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(name))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.type").value(type))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1));
+    }
+
+    @Test
+    @DisplayName("Test tea to return values as specified in the request")
+    public void testGetTea() throws Exception {
+        String type = "green";
+        String name = "Mint";
+
+        this.mockMvcController.perform(
+                        MockMvcRequestBuilders.get("/tea?name=Mint&type=green"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(name))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.type").value(type))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(2));
     }
 }
