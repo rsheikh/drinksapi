090364e63b69386f10d11a28784e6b29e2476aed
rsheikh <red.sheikh@gmail.com>
2024-08-29 12:41:11 +0100

    feat: added new mapping to obtain default and param values from request url

--- src/main/java/com/northcoders/drinksapi/controller/TeaController.java
+++ src/main/java/com/northcoders/drinksapi/controller/TeaController.java
@@ -1,7 +1,10 @@
 package com.northcoders.drinksapi.controller;
 
+import com.northcoders.drinksapi.model.Coffee;
+import com.northcoders.drinksapi.model.Tea;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +14,12 @@ public class TeaController {
     @GetMapping("/tealovers")
     public String teaLover(){
         return "I love tea!";
+    }
+
+    @GetMapping("")
+    public Tea teaname(@RequestParam(defaultValue = "black") String type,
+                       @RequestParam(defaultValue = "Assam") String name) {
+
+        return new Tea(name, type);
     }
 }
