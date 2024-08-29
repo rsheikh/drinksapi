5b7855c0cfcb8caef025dceef0a1a7d9d14514f1
jmc <120180071+sector-11@users.noreply.github.com>
2024-08-29 12:04:15 +0100

    refactor: refactored homecontroller to point old coffee endpoints to new class

--- src/main/java/com/northcoders/drinksapi/controller/HomeController.java
+++ src/main/java/com/northcoders/drinksapi/controller/HomeController.java
@@ -1,14 +1,17 @@
 package com.northcoders.drinksapi.controller;
 
 import com.northcoders.drinksapi.model.Coffee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("/")
+@RequestMapping("/")
 public class HomeController {
+    @Autowired
+    CoffeeController coffeeController;
 
     @GetMapping("/")
     public String home() {
@@ -17,11 +20,11 @@ public class HomeController {
 
     @GetMapping("/coffeelover")
     public String coffeelover(){
-        return "I love coffee!";
+        return coffeeController.coffeelover();
     }
 
     @GetMapping("/coffee")
     public Coffee coffeename(@RequestParam(defaultValue = "latte") String name) {
-        return new Coffee(name);
+        return coffeeController.coffeename(name);
     }
 }
