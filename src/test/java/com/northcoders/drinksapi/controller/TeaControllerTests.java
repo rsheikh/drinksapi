d8c2929f45c560136b61b4bc2d487a6cc53411ba
jmc <120180071+sector-11@users.noreply.github.com>
2024-08-29 12:18:40 +0100

    test: test for tealover endpoint in tea class

---
+++ src/test/java/com/northcoders/drinksapi/controller/TeaControllerTests.java
@@ -0,0 +1,27 @@
+package com.northcoders.drinksapi.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class TeaControllerTests {
+
+    @Autowired
+    private MockMvc mockMvcController;
+
+    @Test
+    public void testGetTeaLover() throws Exception{
+        String expectedContent = "I love tea!";
+
+        this.mockMvcController.perform(
+                MockMvcRequestBuilders.get("/tea/tealovers/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(expectedContent));
+    }
+}
